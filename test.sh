f69293ec8b0b60f34e1dd4f70c5d53ca97cb2e77
wpark <woolim.park@hotmail.com>
2020-04-22 20:54:07 +0200

    f

--- test.sh
+++
@@ -1,4 +0,0 @@
-make re
-gcc  main.c test_basic.c test_x.c test_u.c test_c.c test_s.c test_p.c test_d.c libftprintf.a
-#valgrind  --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose --log-file=valgrind-out.txt ./a.out
-./a.out
